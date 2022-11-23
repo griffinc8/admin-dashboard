08d7aab9c1a3223a524d28baa02633c411cc973e
griffinc8 <griffincoccari@gmail.com>
2022-11-22 20:52:09 -0600

    list iteration inserted into sidebar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 const sidebar_items = ["Home", "Profile", "Messages", "History", "Tasks", "Communities"]
-console.log(sidebar_items.length)
+const sidebar_length = sidebar_items.length
 
 const left_sidebar_container = document.querySelector(".left-sidebar-container")
 console.log(left_sidebar_container)
 
-left_sidebar_container.setAttribute('style', "grid-template-rows: repeat(10, 60px)")
+left_sidebar_container.setAttribute('style', `grid-template-rows: repeat(${sidebar_length}, 60px)`)
+
+for (let i = 0; i < sidebar_length; i++) {
+    console.log(i)
+    const div = document.createElement('div')
+    div.classList.add(`${sidebar_items[i]}`);
+    div.textContent = `${sidebar_items[i]}`
+    left_sidebar_container.appendChild(div)
+}
